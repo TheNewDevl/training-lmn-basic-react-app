83ddcbaf102bc33a0c86e1deb2b275e7b0eff83b
TheNewDevl <95215674+TheNewDevl@users.noreply.github.com>
2022-04-24 14:04:07 +0200

    use stateful component and add style

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,19 @@ import Cart from "./Cart";
 import ShoppingList from "./ShoppingList";
 import logo from '../assets/logo.png'
 import Footer from "./Footer";
-
+import '../styles/Layout.css'
 
 function App() {
   return (
     <div>
-
       <Banner>
         <img src={logo} alt="La maison jungle, Logo" className='lmj-logo' />
         <h1 className="lmj-title">La maison de la Jungle</h1>
       </Banner>
-      <Cart />
-      <ShoppingList />
+      <div className="lmj-layout-inner">
+        <Cart />
+        <ShoppingList />
+      </div>
       <Footer />
     </div>
   );
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,38 @@
 import '../styles/Cart.css'
+import { useState } from 'react'
 
 export default function Cart() {
-    const monsteraPrice = 8
-    const ivyPrice = 10
-    const flowerPrice = 15
+    const [cart, updateCart] = useState(0)
+    const [isOpen, setIsOpen] = useState(false)
 
-    return (
+    const monsteraPrice = 8
+    /* const ivyPrice = 10
+    const flowerPrice = 15 */
+
+    return (isOpen ? (
         <div className="lmj-cart">
+            <button
+                className='lmj-cart-toggle-button'
+                onClick={() => setIsOpen(false)}>Fermer le panier</button>
             <h2>Panier</h2>
             <ul>
                 <li>Monstera : {monsteraPrice}€</li>
-                <li>Lierre : {ivyPrice}€</li>
-                <li>Fleurs : {flowerPrice}€</li>
+                {/*  <li>Lierre : {ivyPrice}€</li>
+                <li>Fleurs : {flowerPrice}€</li> */}
+                <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+
             </ul>
-            Total : {monsteraPrice + ivyPrice + flowerPrice}€
+            Total : {monsteraPrice * cart/*  + ivyPrice + flowerPrice */}€
+            <button onClick={() => updateCart(0)}>Vider le panier</button>
+        </div>
+    ) : (
+        <div className='lmj-cart-closed'>
+
+            <button
+                className='lmj-cart-toggle-button'
+                onClick={() => setIsOpen(true)}>Ouvrir le panier</button>
         </div>
     )
+    )
+
 }
